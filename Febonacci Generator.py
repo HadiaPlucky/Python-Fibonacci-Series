f0986eee1901d5117bb8dd14cb57247192c0ce1e
HadiaPlucky <hadia.plucky@gmail.com>
2024-04-16 10:25:17 +0500

    Final edited code

--- Febonacci Generator.py
+++ Febonacci Generator.py
@@ -1,14 +1,13 @@
-n=int(input("How many Fibonacci Numbers do you want to print\n"))
+n=int(input("How many Fibonacci Numbers do you want to print "))
 if(n==0):
     print("No fibonacci number at zeroth position")
 elif(n==1):
     print("0")
-
-
-a=0
-b=1
-print(a)
-print(b)
+else:
+    a=0
+    b=1
+    print(a)
+    print(b)
 
 for i in range(2,n):
     c=a+b
