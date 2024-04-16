99087615e09b86a40bb36c88fd8f323507e74af2
HadiaPlucky <hadia.plucky@gmail.com>
2024-04-16 10:17:44 +0500

    conditions for 0 and 1 added

--- Febonacci Generator.py
+++ Febonacci Generator.py
@@ -1,4 +1,10 @@
 n=int(input("How many Fibonacci Numbers do you want to print\n"))
+if(n==0):
+    print("No fibonacci number at zeroth position")
+elif(n==1):
+    print("0")
+
+
 a=0
 b=1
 print(a)
