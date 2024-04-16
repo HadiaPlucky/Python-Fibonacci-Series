67fdd34ab94b7a05ab344b5ac817ba3b02f7695b
HadiaPlucky <hadia.plucky@gmail.com>
2024-04-16 08:53:38 +0500

    Done

---
+++ first.py
@@ -0,0 +1,13 @@
+# n=int(input("How many Fibonacci Numbers do you want to print"))
+# a=0
+# b=1
+# print(a)
+# print(b)
+
+# for i in range(2,n):
+#     c=a+b
+#     a=b
+#     b=c
+#     print(c)
+print("HIii")
+
